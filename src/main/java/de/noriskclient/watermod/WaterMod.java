6f8d250548615f0ba2fc754e6c97b3e034a7d98d
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2020-12-26 14:33:07 +0100

    WaterMod Init

---
+++ src/main/java/de/noriskclient/watermod/WaterMod.java
@@ -0,0 +1,10 @@
+package de.noriskclient.watermod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WaterMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
